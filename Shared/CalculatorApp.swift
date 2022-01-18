89a75b06502e2d7a004e79c68f76565fd0528f1d
Charlie <whimthen@gmail.com>
2022-01-18 19:55:28 +0800

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Charlie on 2022/1/18.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
